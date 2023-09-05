cb23ff8faad61257e49c0cd9462fe5a6dee2c28c
Anugrah Mishra <anugrah.mishra@globallogic.com>
2023-09-05 14:42:53 +0530

    Updated events + wrapped calendar in container for auto tooltip position

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,31 @@ function App() {
     {
       name: "Daily",
       dates: [
-        "2023-05-11T00:00:00.000Z",
-        "2023-05-12T00:00:00.000Z",
-        "2023-05-13T00:00:00.000Z",
-        "2023-05-14T00:00:00.000Z",
+        "2023-09-11T00:00:00.000Z",
+        "2023-09-12T00:00:00.000Z",
+        "2023-10-13T00:00:00.000Z",
+        "2024-05-13T00:00:00.000Z",
+        "2023-12-14T00:00:00.000Z",
       ],
       color: "#a6fbcc",
     },
     {
       name: "Weekly",
       dates: [
-        "2023-05-19T00:00:00.000Z",
-        "2023-05-26T00:00:00.000Z",
-        "2023-06-02T00:00:00.000Z",
+        "2023-09-19T00:00:00.000Z",
+        "2024-04-26T00:00:00.000Z",
+        "2024-02-02T00:00:00.000Z",
       ],
       color: "blue",
     },
   ];
   return (
-    <YearlyCalendar
-      eventList={events}
-      onDayClick={(a: string) => console.log("clicked", a)}
-    />
+    <div className="schedule-calendar-container">
+      <YearlyCalendar
+        eventList={events}
+        onDayClick={(a: string) => console.log("clicked", a)}
+      />
+    </div>
   );
 }
 
